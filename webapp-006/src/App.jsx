87984675c4df137a2ceaa1504f3cb375e4fec250
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-07-19 09:42:05 -0300

    Update

--- webapp-006/src/App.jsx
+++ webapp-006/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Componentes
+import Header from './Components/Header';
 import Home from './Components/Home';
 import Services from './Components/Services';
 import Contacts from './Components/Contacts';
@@ -13,22 +14,7 @@ import About from "./Components/About";
 export default function App() {
   return (
     <>
-    <div className="container-fluid- bg-dark text-white p-2 text-center">     
-      <div className="row">
-        <div className="col">
-          <h3>React Router App!</h3>
-        </div>
-      </div>
-    </div>
-     <nav className="container mt-3">
-      <div className="row">
-        <div className="col">
-          <Link className="btn btn-primary me-3" to="/">Home</Link> 
-          <Link className="btn btn-primary me-3" to="/services">Serviços</Link> 
-          <Link className="btn btn-primary" to="/contacts">Contatos</Link>
-          </div>
-      </div>       
-     </nav> 
+    <Header/>
      <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/services' element={<Services />} />
