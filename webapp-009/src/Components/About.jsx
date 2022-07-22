988c3d30afa42f66b9cfda7db1ad80ee01205d63
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-07-22 10:14:59 -0300

    Update

--- webapp-009/src/Components/About.jsx
+++ webapp-009/src/Components/About.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from 'react-router-dom';
 
 export default function About() {
+
+    const [nomes] = useState([
+      'Daniel',
+      'Priscila',
+      'Sapup3'
+    ]);
+
+
     return (
         <section className="container">
           <div className="row justify-content-center mt-3">
             <div className="col-8 card bg-info text-black text-center p-3">
-              <h5>Autor:</h5>
-              <h4>Daniel Oliveira</h4>             
+              <h5>Cliente:</h5>
+              <h4>{nomes[0]}</h4>             
             </div>
             <div className="col-12 text-center mt-3">
               <Link to={'/'}>Home</Link>
