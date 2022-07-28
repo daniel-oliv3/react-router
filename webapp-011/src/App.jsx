d95e67d69df375f1fb2569a9618c81ecd625949b
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-07-28 09:41:04 -0300

    Update

--- webapp-011/src/App.jsx
+++ webapp-011/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,6 +15,15 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
+        <Route path="servicos" element={<h1>Serviços</h1>} />
+        <Route path="contatos" element={<><h1>Contatos</h1><Outlet /></>} >
+          <Route path="localizacao" element={<h1>Localização</h1>} />
+          <Route path="telefone" element={<h1>Telefone</h1>} />
+          <Route path="email" element={<h1>E-mail</h1>} />
+          <Route path="*" element={<h1>ERRO!</h1>} />
+        </Route>
+
+        <Route path="*" element={<h1>ERRO!</h1>} />
       </Routes>
     </>
   );
