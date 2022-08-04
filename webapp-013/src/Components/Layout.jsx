3ab7763df1c7730b415c5f044bfb668257a42436
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-04 09:24:30 -0300

    Update

--- webapp-013/src/Components/Layout.jsx
+++ webapp-013/src/Components/Layout.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from 'react-router-dom';
 
-
 export default function Layout() {
   return (
     <>
-
         <nav>
             Navegação
         </nav>
--- webapp-015/src/App.jsx
+++ webapp-015/src/App.jsx
@@ -27,3 +27,8 @@ export default function App() {
   );
 }
 
+
+
+
+
+
